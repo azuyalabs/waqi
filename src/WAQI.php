c770a95abb7e2c93682ba54cfdffa6aad4a9df55
Sacha Telgenhof <stelgenhof@gmail.com>
2017-02-21 17:09:37 +0900

    Minor tweaks.

--- src/WAQI.php
+++ src/WAQI.php
@@ -89,9 +89,9 @@ class WAQI
 
         $_response_body = json_decode($response->getBody());
 
-        if ($_response_body->status == 'ok') {
+        if ($_response_body->status === 'ok') {
             $this->raw_data = $_response_body->data;
-        } elseif ($_response_body->status == 'error') {
+        } elseif ($_response_body->status === 'error') {
             switch ($_response_body->data) {
                 case 'Unknown station':
                     throw new UnknownStationException($station);
@@ -126,35 +126,35 @@ class WAQI
         $narrative_cautionary = '';
 
         // AQI Level: Good
-        if ($aqi >= 0 and $aqi <= 50) {
+        if ($aqi >= 0 && $aqi <= 50) {
             $narrative_level = 'Good';
             $narrative_health = 'Air quality is considered satisfactory, and air pollution poses little or no risk.';
             $narrative_cautionary = 'None';
         }
 
         // AQI Level: Moderate
-        if ($aqi >= 51 and $aqi <= 100) {
+        if ($aqi >= 51 && $aqi <= 100) {
             $narrative_level = 'Moderate';
             $narrative_health = 'Air quality is acceptable, however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.';
             $narrative_cautionary = 'Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.';
         }
 
         // AQI Level: Unhealthy for sensitive groups
-        if ($aqi >= 101 and $aqi <= 150) {
+        if ($aqi >= 101 && $aqi <= 150) {
             $narrative_level = 'Unhealthy for Sensitive Groups';
             $narrative_health = 'Members of sensitive groups may experience health effects. The general public is not likely to be affected.';
             $narrative_cautionary = 'Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.';
         }
 
         // AQI Level: Unhealthy
-        if ($aqi >= 151 and $aqi <= 200) {
+        if ($aqi >= 151 && $aqi <= 200) {
             $narrative_level = 'Unhealthy';
             $narrative_health = 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.';
             $narrative_cautionary = 'Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion.';
         }
 
         // AQI Level: Very unhealthy
-        if ($aqi >= 201 and $aqi <= 300) {
+        if ($aqi >= 201 && $aqi <= 300) {
             $narrative_level = 'Very Unhealthy';
             $narrative_health = 'Health warnings of emergency conditions. The entire population is more likely to be affected.';
             $narrative_cautionary = 'Active children and adults, and people with respiratory disease, such as asthma, should avoid all outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion.';
