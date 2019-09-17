688572f54923f5e495e701d8e25e8ae97c50ce87
Sacha Telgenhof <stelgenhof@gmail.com>
2019-09-18 00:15:32 +0900

    Dropped unnecessary arguments as they are the same as the default.

--- src/Exceptions/InvalidAccessTokenException.php
+++ src/Exceptions/InvalidAccessTokenException.php
@@ -24,6 +24,6 @@ class InvalidAccessTokenException extends Exception
      */
     public function __construct()
     {
-        parent::__construct('Invalid access token', 0, null);
+        parent::__construct('Invalid access token');
     }
 }
--- src/Exceptions/QuotaExceededException.php
+++ src/Exceptions/QuotaExceededException.php
@@ -28,6 +28,6 @@ class QuotaExceededException extends Exception
      */
     public function __construct()
     {
-        parent::__construct('Request Quota has been exceeded', 0, null);
+        parent::__construct('Request Quota has been exceeded');
     }
 }
