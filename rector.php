0aafa29597814c71a22367b6ea3bea6e8b260ec3
Sacha Telgenhof <me@sachatelgenhof.com>
2023-05-21 20:35:08 +0900

    Fix import order.
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- rector.php
+++ rector.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
