3bab46c9e0b0b6947be5656f6802cf16fda094bc
Sacha Telgenhof <me@sachatelgenhof.com>
2025-01-06 00:33:26 +0900

    docs: update copyright year
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- rector.php
+++ rector.php
@@ -7,7 +7,7 @@ declare(strict_types = 1);
  *
  * Simple PHP Wrapper for the World Air Quality Index API.
  *
- * Copyright (c) 2017 - 2024 AzuyaLabs
+ * Copyright (c) 2017 - 2025 AzuyaLabs
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
