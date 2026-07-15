6b34a093f71da3f3232ca6432edb3fee8f1303bf
Sacha Telgenhof <me@sachatelgenhof.com>
2026-07-15 22:25:23 +0900

    build(rector): update configuration to latest syntax
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

--- rector.php
+++ rector.php
@@ -16,19 +16,19 @@ declare(strict_types = 1);
  */
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    ->withSets([
-        LevelSetList::UP_TO_PHP_83,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-        SetList::INSTANCEOF,
-    ]);
+    ->withParallel()
+    ->withFluentCallNewLine()
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        earlyReturn: true,
+    )
+    ->withComposerBased(phpunit: true);
